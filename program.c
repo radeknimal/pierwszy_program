7917d7db65ddae48ae5d594b4497db81fb8643c3
radeknimal <radek.nimal@gmail.com>
2017-10-19 16:04:51 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* to sa komentarze */
+/* wszystko w C jest funkcja  y=f(x) */
+
+
+int main() /* main to glowna funckja, moze byc tylko jedna, zawsze zwraca liczbe calkowita */
+{ /* miedzy klamrami jest cialo funkcji, czy to co bedzie wykonane */
+
+	puts("Pierwszy program"); /* funkcja z bibliotekami stdio, cztli standardowej biblioteki wejscia wyjcia */
+
+	return 0; /* main zwraca wartosc zero */
+
+}
+
